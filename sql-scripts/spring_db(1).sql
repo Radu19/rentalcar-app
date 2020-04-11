15325d287912e6c70e41be9c5200b87886f6c621
radu2020 <radu.ask.work@gmail.com>
2020-04-11 11:47:14 +0300

    *Updated index.html, car-form.html *Updated entity/Car.java *Added sql-scripts/spring_db(1).sql

---
+++ sql-scripts/spring_db(1).sql
@@ -0,0 +1,247 @@
+-- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
+--
+-- Host: localhost    Database: spring_db
+-- ------------------------------------------------------
+-- Server version	8.0.19
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `car`
+--
+
+DROP TABLE IF EXISTS `car`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `car` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `brand` varchar(45) NOT NULL,
+  `model` varchar(45) NOT NULL,
+  `segment` varchar(45) DEFAULT NULL,
+  `production_year` int NOT NULL,
+  `car_body` varchar(45) NOT NULL,
+  `color` varchar(45) NOT NULL,
+  `nr_of_doors` int NOT NULL,
+  `nr_of_seats` int NOT NULL,
+  `transmission` varchar(45) NOT NULL,
+  `fuel` varchar(45) NOT NULL,
+  `engine` double NOT NULL,
+  `features` longtext NOT NULL,
+  `casco` double NOT NULL,
+  `luggage` int NOT NULL,
+  `status` varchar(45) NOT NULL,
+  `img` varchar(45) NOT NULL,
+  `price1` double NOT NULL,
+  `price2` double NOT NULL,
+  `price3` double NOT NULL,
+  `price4` double NOT NULL,
+  `price5` double NOT NULL,
+  `weight` int NOT NULL,
+  `traction` varchar(45) NOT NULL,
+  `minimal_age` int NOT NULL,
+  `driving_exp` int NOT NULL,
+  `garant` double NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `car`
+--
+
+LOCK TABLES `car` WRITE;
+/*!40000 ALTER TABLE `car` DISABLE KEYS */;
+INSERT INTO `car` VALUES (1,'Audi','Q7','J',2017,'Hatchback','Black',5,5,'Manual','Diesel',2.3,'Air conditioning, Cruise Control, MP3/AUX/USB, ABS/TC/ESP, Steering Controls, 8 airbags',12,5,'Available','audi-q7-id1.jpg',78,73,68,63,55,2410,'AWD',25,3,950),(2,'Audi','A8 ','F',2018,'Sedan','Gray',5,5,'Automatic','Petrol',3.5,'Air conditioning, Cruise Control, MP3/AUX/USB, Heating Seats, Steering Controls, 10 airbags',30,4,'Available','audi-a8-id2.jpg',150,145,140,135,125,1945,'RWD',26,4,2500),(3,'BMW','X6 ','J',2015,'Hatchback','Red',5,5,'Manual','Diesel',3.3,'Climat Control, ABS, EBD, AM/FM Radio, MP3 Player/USB, 6 airbags, 2WD, Heated seats',15,4,'Available','bmw-x6-id3.jpg',80,77,73,68,60,2340,'AWD',24,3,900),(4,'Mercedes','S Class','F',2018,'Sedan','Black',5,5,'Automatic','Diesel',3.5,'Climat Control, ABS, EBD, AM/FM Radio, MP3 Player/USB, 12 airbags, 2WD, Heated seats',25,3,'Available','s-class-id4.jpg',90,87,85,80,72,2275,'RWD',26,5,2200),(5,'Jaguar','XF','F',2015,'Sedan','Gray',5,5,'Automatic','Petrol',2.8,'Audio System, Climat Control, ABS, EBD, AM/FM Radio, MP3 Player/USB, 4 airbags, 2WD, Heated seats',15,3,'Not Available','jaguar-xf-id5.jpg',75,73,70,64,55,2400,'FWD',25,3,700),(6,'Porshe','Panamera','S',2013,'Cabriolet','Brown',5,5,'Automatic','Petrol',4.1,'Climat Control, ABS, EBD, AM/FM Radio, MP3 Player/USB, 6 airbags, 2WD, Heated seats',20,2,'Not Available','panamera-id6.jpg',85,82,80,75,65,1800,'AWD',25,3,800);
+/*!40000 ALTER TABLE `car` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `contact`
+--
+
+DROP TABLE IF EXISTS `contact`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `contact` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `phone_nr` varchar(45) NOT NULL,
+  `message` longtext NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `contact`
+--
+
+LOCK TABLES `contact` WRITE;
+/*!40000 ALTER TABLE `contact` DISABLE KEYS */;
+/*!40000 ALTER TABLE `contact` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `customer`
+--
+
+DROP TABLE IF EXISTS `customer`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `customer` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(45) NOT NULL,
+  `last_name` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `phone_nr` int NOT NULL,
+  `address` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `customer`
+--
+
+LOCK TABLES `customer` WRITE;
+/*!40000 ALTER TABLE `customer` DISABLE KEYS */;
+/*!40000 ALTER TABLE `customer` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `order`
+--
+
+DROP TABLE IF EXISTS `order`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `order` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `order_date` date NOT NULL,
+  `start_date` date NOT NULL,
+  `end_date` date NOT NULL,
+  `hire_days` int NOT NULL,
+  `total_cost` float NOT NULL,
+  `status` varchar(45) NOT NULL,
+  `payment_status` varchar(45) NOT NULL,
+  `payment_method` varchar(45) NOT NULL,
+  `car_id` int NOT NULL,
+  `customer_id` int NOT NULL,
+  `user_id` int NOT NULL,
+  PRIMARY KEY (`id`,`car_id`,`customer_id`,`user_id`),
+  KEY `fk_order_car1_idx` (`car_id`),
+  KEY `fk_order_customer1_idx` (`customer_id`),
+  KEY `fk_order_user1_idx` (`user_id`),
+  CONSTRAINT `fk_order_car1` FOREIGN KEY (`car_id`) REFERENCES `car` (`id`),
+  CONSTRAINT `fk_order_customer1` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`id`),
+  CONSTRAINT `fk_order_user1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `order`
+--
+
+LOCK TABLES `order` WRITE;
+/*!40000 ALTER TABLE `order` DISABLE KEYS */;
+/*!40000 ALTER TABLE `order` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `role`
+--
+
+DROP TABLE IF EXISTS `role`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `role` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `role`
+--
+
+LOCK TABLES `role` WRITE;
+/*!40000 ALTER TABLE `role` DISABLE KEYS */;
+INSERT INTO `role` VALUES (1,'ROLE_EMPLOYEE'),(2,'ROLE_MANAGER'),(3,'ROLE_ADMIN');
+/*!40000 ALTER TABLE `role` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `password` char(80) NOT NULL,
+  `first_name` varchar(50) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'john','$2a$04$eFytJDGtjbThXa80FyOOBuFdK2IwjyWefYkMpiBEFlpBwDH.5PM0K','John','Doe','john@luv2code.com'),(2,'mary','$2a$04$eFytJDGtjbThXa80FyOOBuFdK2IwjyWefYkMpiBEFlpBwDH.5PM0K','Mary','Public','mary@luv2code.com'),(3,'susan','$2a$04$eFytJDGtjbThXa80FyOOBuFdK2IwjyWefYkMpiBEFlpBwDH.5PM0K','Susan','Adams','susan@luv2code.com'),(4,'radu','$2a$10$.w5fk/dQTR0VbcZCUuC7IeQI1oP7PkL9oTi8mVl94Z.3pZgfxZzvi','Radu','Castravet','radu.ask@gmail.com'),(5,'olesea','$2a$10$R9w3r3XUZA21XeTDgs3jn.6kG2n9CS3rbn8Cm6ovgAkk5vRyGGrDe','Olesea','Goreanu','goreanuolesea@gmail.com');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users_roles`
+--
+
+DROP TABLE IF EXISTS `users_roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users_roles` (
+  `user_id` int NOT NULL,
+  `role_id` int NOT NULL,
+  PRIMARY KEY (`user_id`,`role_id`),
+  KEY `FK_ROLE_idx` (`role_id`),
+  CONSTRAINT `FK_ROLE` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`),
+  CONSTRAINT `FK_USER_05` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users_roles`
+--
+
+LOCK TABLES `users_roles` WRITE;
+/*!40000 ALTER TABLE `users_roles` DISABLE KEYS */;
+INSERT INTO `users_roles` VALUES (1,1),(2,1),(3,1),(4,1),(5,1),(2,2),(3,2),(3,3);
+/*!40000 ALTER TABLE `users_roles` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-04-11 11:44:35

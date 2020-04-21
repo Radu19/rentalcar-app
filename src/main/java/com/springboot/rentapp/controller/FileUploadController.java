80a0a74493e1946d30109513b534cab3ac9f28f3
radu2020 <radu.ask@gmail.com>
2020-04-21 22:36:46 +0300

    Update FileUploadController

--- src/main/java/com/springboot/rentapp/controller/FileUploadController.java
+++ src/main/java/com/springboot/rentapp/controller/FileUploadController.java
@@ -1,10 +1,12 @@
 package com.springboot.rentapp.controller;
 
+import java.io.File;
 import java.io.IOException;
 
 import com.springboot.rentapp.storage.StorageFileNotFoundException;
 import com.springboot.rentapp.storage.StorageService;
 
+import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
@@ -49,12 +51,16 @@ public class FileUploadController {
 
 	@PostMapping("/image")
 	public String handleFileUpload(@RequestParam("file") MultipartFile file,
-			RedirectAttributes redirectAttributes) {
+			RedirectAttributes redirectAttributes) throws IOException {
 
 		storageService.store(file);
-		redirectAttributes.addFlashAttribute("message", file.getOriginalFilename());
+		redirectAttributes.addFlashAttribute("carName", file.getOriginalFilename());
 
-		return "redirect:/upload/image";
+		String directoryString = "src/main/resources/static/img/docs";
+		File theFile = new File(directoryString, file.getOriginalFilename());
+		FileUtils.writeByteArrayToFile(theFile, file.getBytes());
+
+		return "redirect:/cars/showFormForAdd";
 	}
 
 	@ExceptionHandler(StorageFileNotFoundException.class)

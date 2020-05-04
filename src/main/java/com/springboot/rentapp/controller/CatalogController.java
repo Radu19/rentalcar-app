1a85728ee705eac3ef2bffb028aac491b4755e41
radu2020 <radu.ask@gmail.com>
2020-05-04 21:43:55 +0300

    Create car-view.html

--- src/main/java/com/springboot/rentapp/controller/CatalogController.java
+++ src/main/java/com/springboot/rentapp/controller/CatalogController.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.springboot.rentapp.entity.Car;
 import com.springboot.rentapp.service.CarService;
@@ -30,4 +32,21 @@ public class CatalogController {
 
 		return "/catalog/list-catalog";
 	}
+	
+	@GetMapping("/view")
+	public String viewCar(@RequestParam("carId") int theId, Model theModel) {
+		
+		Car theCar = carService.findById(theId);
+		
+		theModel.addAttribute("car", theCar);
+		
+		return "/catalog/car-view";
+	}
+	
+	@PostMapping("/book")
+	public String bookCar() {
+		return null;
+	}
+	
+	
 }
